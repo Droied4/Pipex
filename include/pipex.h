aa898d74b56a7d48886cd8d412b1cf93e7d6a4d6
Carmen Ordoñez <deiord44santiagoov@gmail.com>
2024-01-12 20:13:01 +0100

    ok

--- include/pipex.h
+++ include/pipex.h
@@ -20,6 +20,7 @@
 # include <stdlib.h>
 # include <string.h>
 # include <sys/wait.h>
+# include <sys/stat.h>
 # include <unistd.h>
 
 typedef struct s_pipe
--- src/pipex.c
+++ src/pipex.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "../include/pipex.h"
 
 /* auxiliar
 void	ft_print_info(t_pipe *info)
